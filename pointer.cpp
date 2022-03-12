18adc3c990bc8384abba8442dcdd586ff7919c50
Nhat Tuan <nhattuan@Nhat-Tuan-IT.local>
2022-03-12 14:35:11 +0700

    them va xoa phan tu mang 1 chieu

--- pointer.cpp
+++ pointer.cpp
@@ -7,18 +7,116 @@ void setToNull(int*& iPtr)
     iPtr = nullptr;
 }
 
+void arrInput(int* iArr, int iLength)
+{
+    for (int i = 0; i < iLength; i++)
+    {
+        cout << "a[" << i << "]: ";
+        cin >> iArr[i]; 
+    }
+}
+
+void arrOutput(const int* iArr, int iLength)
+{
+    for (int i = 0; i < iLength; i++)
+    {
+        cout << iArr[i] << " ";
+    }
+}
+
+void arrChangeTheSize(int*& iArr, int iOldSize, int iNewSize)
+{
+    // Bước 1: Cấp phát mảng tạm và đỗ dữ liệu sang mảng tạm.
+    int* iArrTemp = new int[iOldSize];
+    for (int i = 0; i < iOldSize; i++)
+    {
+        iArrTemp[i] = iArr[i];
+    }
+    
+    // Bước 2: Giải phóng mảng hiện tại và cấp phát với kích thước mới.
+    delete[] iArr;
+    iArr = new int[iNewSize];
+
+    // Bước 3: Đỗ dữ liệu từ mảng tạm sang mảng hiện tại với kích thước mới.
+    int iMinSize = iOldSize < iNewSize ? iOldSize : iNewSize;
+    for (int i = 0; i < iMinSize; i++)
+    {
+        iArr[i] = iArrTemp[i];
+    }
+
+    // Bước 4: Giải phóng mảng tạm.
+    delete[] iArrTemp;
+}
+
+void arrAddElement(int*& iArr, int& iLength, int iAddLocation, int iAddElement)
+{
+    /*
+    a:      1 2 3 4 5
+    index:  0 1 2 3 4
+    vị trí thêm là 2
+    phần tử thêm là 69
+    
+    1
+    a:      1 2 3 4 5 0
+    index:  0 1 2 3 4 5
+
+    2
+    a5 = a4
+    a4 = a3
+    a3 = a2
+    a2 = 69
+
+    3
+    a:      1 2 69 3 4 5
+    index:  0 1 2 3 4 5
+
+    4
+    length + 1
+    */
+    arrChangeTheSize(iArr, iLength, iLength + 1);
+    for (int i = iLength - 1; i >= iAddLocation; --i)
+    {
+        iArr[i + 1] = iArr[i];
+    }
+    iArr[iAddLocation] = iAddElement;
+    ++iLength;
+}
+
+void arrDeleteElement(int*& iArr, int& iLength, int iDeleteLocation)
+{
+    /*
+    a:      1 2 3 4 5
+    index:  0 1 2 3 4
+
+    vị trí xoá: 2
+    a2 = a3
+    a3 = a4
+    =>  a:   1 2 4 5 5
+    index:   0 1 2 3 4
+    */
+    for (int i = iDeleteLocation + 1; i < iLength; ++i)
+    {
+        iArr[i - 1] = iArr[i];
+    }
+    arrChangeTheSize(iArr, iLength, iLength);
+    iLength--;
+}
+
 int main()
 {
-    int iA = 5;
-    int* iPtr = &iA;
-    cout << "Chua gan null: " << iPtr << "\n";
-    setToNull(iPtr);
-    if (iPtr == nullptr)
-    {
-        cout << "Set null\n";
-    }
-    else
-    {
-        cout << "Not null: " << iPtr << "\n";
-    }
+    int iLength = 5;
+    int* iArr = new int[iLength];
+    arrInput(iArr, iLength);
+    cout << "Mang sau khi nhap: ";
+    arrOutput(iArr, iLength);
+
+    // arrAddElement(iArr, iLength, 2, 69);
+    // cout << "\nMang sau khi them phan tu 69: ";
+    // arrOutput(iArr, iLength);
+
+    arrDeleteElement(iArr, iLength, 2);
+    cout << "\nMang sau khi xoa phan tu vi tri 2: ";
+    arrOutput(iArr, iLength);
+
+    delete[] iArr;
 }
