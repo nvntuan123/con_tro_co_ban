9cb0e6b75c84c2082ab1fb9c647d283851349725
Nhat Tuan <nvntuan123@gmail.com>
2022-03-15 00:11:00 +0700

    thay doi kich thuoc mang 2 chieu

--- pointer.cpp
+++ pointer.cpp
@@ -3,6 +3,62 @@
 
 using namespace std;
 
+void MatrixAllocationMemory(int**& iMatrix, int iRows, int iCols)
+{
+    iMatrix = new int*[iRows];
+    for (size_t i = 0; i < iRows; i++)
+    {
+        iMatrix[i] = new int[iCols];
+    }
+}
+
+void MatrixFree(int**& iMatrix, int iRows, int iCols)
+{
+    for (size_t i = 0; i < iRows; i++)
+    {
+        delete[] iMatrix[i];
+    }
+    delete[] iMatrix;
+}
+
+void MatrixDataCopy(int** iMatrixSource, int** iMatrixDest, int iRows, int iCols)
+{
+    for (size_t i = 0; i < iRows; i++)
+    {
+        for (size_t j = 0; j < iCols; j++)
+        {
+            iMatrixDest[i][j] = iMatrixSource[i][j];
+        }
+    }    
+}
+
+// bCheck = true ? Row : Col
+void MatrixREADLOC(int**& iMatrix, int iRowOldSize, int iColOldSize, int iNewSize, bool bCheck)
+{
+    // B1: Tạo mảng tạm và đỗ dữ liệu từ mảng chính sang tạm.
+    // Cấp phát mảng tạm.
+    int** iMatrixTemp;
+    MatrixAllocationMemory(iMatrixTemp, iRowOldSize, iColOldSize);
+
+    // Đỗ dữ liệu từ mảng chính sang mảng tạm.
+    MatrixDataCopy(iMatrix, iMatrixTemp, iRowOldSize, iColOldSize);
+
+    // B2: Giải phóng mảng hiện tại và cấp phát với kích thước mới.
+    MatrixFree(iMatrix, iRowOldSize, iColOldSize);
+    MatrixAllocationMemory(iMatrix, iRowOldSize, iColOldSize);
+
+    // B3: Đỗ dữ liệu từ mảng tạm sang mảng chính.
+    MatrixDataCopy(
+        iMatrixTemp,
+        iMatrix,
+        (bCheck ? (iRowOldSize < iNewSize ? iRowOldSize : iNewSize) : iRowOldSize),
+        (bCheck ? iColOldSize : (iColOldSize < iNewSize ? iColOldSize : iNewSize))
+        );
+
+    // B4: Giải phóng mảng tạm.
+    MatrixFree(iMatrixTemp, iRowOldSize, iColOldSize);
+}
+
 void MatrixRandomElement(int** iMatrix, int iRows, int iCols)
 {
     srand(time(0)); // Reset time.
