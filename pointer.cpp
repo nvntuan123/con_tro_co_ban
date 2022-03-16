4da4f264e37080574ccc8a9f7429201bcf71c624
Nhat Tuan <nvntuan123@gmail.com>
2022-03-16 14:27:41 +0700

    add, delete row col

--- pointer.cpp
+++ pointer.cpp
@@ -53,10 +53,75 @@ void MatrixREADLOC(int**& iMatrix, int iRowOldSize, int iColOldSize, int iNewSiz
         iMatrix,
         (bCheck ? (iRowOldSize < iNewSize ? iRowOldSize : iNewSize) : iRowOldSize),
         (bCheck ? iColOldSize : (iColOldSize < iNewSize ? iColOldSize : iNewSize))
-        );
+    );
 
     // B4: Giải phóng mảng tạm.
     MatrixFree(iMatrixTemp, iRowOldSize, iColOldSize);
+}
+
+void MatrixRowAdd(int**& iMatrix, int& iRows, int iCols, int iRowInsertIndex, int* iRowArr)
+{
+    MatrixREADLOC(iMatrix, iRows, iCols, iRows + 1, true);
+    int i, j;
+    for (i = iRows - 1; i > iRowInsertIndex; i--)
+    {
+        for (j = 0; j < iCols; j++)
+        {
+            iMatrix[i + 1][j] = iMatrix[i][j];
+        }
+    }
+    
+    for (j = 0; j < iCols; j++)
+    {
+        iMatrix[iRowInsertIndex][j] = iRowArr[j];
+    }
+    iRows++;
+}
+
+void MatrixRowDelete(int**& iMatrix, int& iRows, int iCols, int iRowDeleteIndex)
+{
+    int i, j;
+    for (i = iRowDeleteIndex + 1; i < iRows; i++)
+    {
+        for (j = 0; j < iCols; j++)
+        {
+            iMatrix[i - 1][j] = iMatrix[i][j];
+        }    
+    }
+    MatrixREADLOC(iMatrix, iRows, iCols, iRows, true);
+    iRows--;
+}
+
+void MatrixColAdd(int**& iMatrix, int iRows, int& iCols, int iColInsertIndex, int* iColArr)
+{
+    MatrixREADLOC(iMatrix, iRows, iCols, iCols + 1, false);
+    int i, j;
+    for (i = iCols - 1; i > iColInsertIndex; i--)
+    {
+        for (j = 0; j < iRows; j++)
+        {
+            iMatrix[i][j + 1] = iMatrix[i][j];
+        }
+    }
+
+    for (i = 0; i < iRows; i++)
+    {
+        iMatrix[i][iColInsertIndex] = iColArr[i];
+    }
+    iCols++;
+}
+
+void MatrixColDelete(int**& iMatrix, int iRows, int& iCols, int iColDeleteIndex)
+{
+    int i, j;
+    for (i = iColDeleteIndex + 1; i < iCols; i++)
+    {
+        for (j = 0; j < iRows; j++)
+        {
+            iMatrix[i][j - 1] = iMatrix[i][j];
+        }
+    }
+    MatrixREADLOC(iMatrix, iRows, iCols, iCols--, false);
 }
 
 void MatrixRandomElement(int** iMatrix, int iRows, int iCols)
